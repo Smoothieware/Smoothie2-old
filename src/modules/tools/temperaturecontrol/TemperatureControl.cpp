2a62c0920f3ca92f2dcef38f9addab851bf2e3d6
Arthur Wolf <wolf.arthur@gmail.com>
2015-09-17 23:44:17 +0200

    temperaturecontrol now ignores M105s with a S parameter

--- src/modules/tools/temperaturecontrol/TemperatureControl.cpp
+++ src/modules/tools/temperaturecontrol/TemperatureControl.cpp
@@ -205,6 +205,7 @@ void TemperatureControl::on_gcode_received(void *argument)
     if (gcode->has_m) {
 
         if( gcode->m == this->get_m_code ) {
+            if(gcode->has_letter('S')){ return; }
             char buf[32]; // should be big enough for any status
             int n = snprintf(buf, sizeof(buf), "%s:%3.1f /%3.1f @%d ", this->designator.c_str(), this->get_temperature(), ((target_temperature == UNDEFINED) ? 0.0 : target_temperature), this->o);
             gcode->txt_after_ok.append(buf, n);
