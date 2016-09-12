e65bc61c942f70ab93720afbe8791f807c9665ee
Douglas Pearless <Douglas.Pearless@pearless.co.nz>
2016-09-12 15:08:53 +1200

    modified:   src/modules/tools/distanceprobes/DistanceProbes.cpp 	modified:   src/modules/tools/distanceprobes/DistanceProbes.h 	modified:   src/modules/tools/distanceprobes/DistanceProbesPublicAccess.h 	modified:   src/modules/tools/distanceprobes/DistanceSensor.h 	modified:   src/modules/tools/distanceprobes/InfraredProbe.cpp 	modified:   src/modules/tools/distanceprobes/InfraredProbe.h

--- src/modules/tools/distanceprobes/DistanceProbesPublicAccess.h
+++ src/modules/tools/distanceprobes/DistanceProbesPublicAccess.h
@@ -1,9 +1,9 @@
 /*
- * DistanceProbesPublicAccess.h
- *
- *  Created on: 8/09/2016
- *      Author: douglaspearless
- */
+    This file is part of Smoothie (http://smoothieware.org/). The motion control part is heavily based on Grbl (https://github.com/simen/grbl).
+    Smoothie is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+    Smoothie is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along with Smoothie. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef SRC_MODULES_TOOLS_DISTANCEPROBES_DISTANCEPROBESPUBLICACCESS_H_
 #define SRC_MODULES_TOOLS_DISTANCEPROBES_DISTANCEPROBESPUBLICACCESS_H_
