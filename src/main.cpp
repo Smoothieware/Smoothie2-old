2f4c5c08f1522748915f0af8730ff12b5f31dbcd
Douglas Pearless <Douglas.Pearless@pearless.co.nz>
2016-10-31 11:18:32 +1300

    modified:   main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,35 @@
-/* Test which brings default HelloWorld project from mbed online compiler
-   to be built under GCC.
- */
-#include "mbed.h"
-#include "Kernel.h"
-#include "SwitchPool.h"
-#include "TemperatureControlPool.h"
-//#include "RotaryDeltaCalibration.h"
-#include "Endstops.h"
-#include "Laser.h"
+/*
+      This file is part of Smoothie (http://smoothieware.org/). The motion control part is heavily based on Grbl (https://github.com/simen/grbl).
+      Smoothie is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+      Smoothie is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+      You should have received a copy of the GNU General Public License along with Smoothie. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "libs/Kernel.h"
+
+#include "modules/tools/laser/Laser.h"
+#include "modules/tools/spindle/Spindle.h"
+#include "modules/tools/extruder/ExtruderMaker.h"
+#include "modules/tools/temperaturecontrol/TemperatureControlPool.h"
+#include "modules/tools/endstops/Endstops.h"
+#include "modules/tools/zprobe/ZProbe.h"
+#include "modules/tools/scaracal/SCARAcal.h"
+#include "RotaryDeltaCalibration.h"
+#include "modules/tools/switch/SwitchPool.h"
+#include "modules/tools/temperatureswitch/TemperatureSwitch.h"
+#include "modules/tools/drillingcycles/Drillingcycles.h"
+#include "FilamentDetector.h"
+#include "MotorDriverControl.h"
+
+#include "modules/robot/Conveyor.h"
+//#include "modules/utils/simpleshell/SimpleShell.h"
+#include "modules/utils/configurator/Configurator.h"
+#include "modules/utils/currentcontrol/CurrentControl.h"
+//#include "modules/utils/player/Player.h"
+#include "modules/utils/killbutton/KillButton.h"
+#include "modules/utils/PlayLed/PlayLed.h"
+//#include "modules/utils/panel/Panel.h"
+//#include "libs/Network/uip/Network.h"
 #include "Config.h"
 #include "checksumm.h"
 #include "ConfigValue.h"
@@ -29,8 +51,17 @@
 //#include "libs/USBDevice/DFU.h"
 //#include "libs/SDFAT.h"
 #include "StreamOutputPool.h"
-#include "MotorDriverControl.h"
+#include "ToolManager.h"
 #include "SEGGER_SYSVIEW.h"
+
+#include "version.h"
+
+#include "mbed.h"
+
+#define second_usb_serial_enable_checksum  CHECKSUM("second_usb_serial_enable")
+#define disable_msd_checksum  CHECKSUM("msd_disable")
+#define dfu_enable_checksum  CHECKSUM("dfu_enable")
+#define watchdog_timeout_checksum  CHECKSUM("watchdog_timeout")
 
 DigitalOut leds[4] = {
 		DigitalOut(LED1),
@@ -39,36 +70,177 @@ DigitalOut leds[4] = {
 		DigitalOut(LED4)
 };
 
-int main() {
+void init() {
 	SEGGER_SYSVIEW_Conf();
-	int cnt = 0;
 
 	// Kernel creates modules, and receives and dispatches events between them
 	Kernel* kernel = new Kernel();
 
-	// Say hello ( TODO: Add back version and all )
-	kernel->streams->printf("Smoothie2 dev\n");
+    kernel->streams->printf("Smoothie Running @%ldMHz\r\n", SystemCoreClock / 1000000);
+    Version version;
+    kernel->streams->printf("  Build version %s, Build date %s\r\n", version.get_build(), version.get_build_date());
+#ifdef CNC
+    kernel->streams->printf("  CNC Build\r\n");
+#endif
+
+bool sdok= false; //TODO remove once SD Card code is working
+//    bool sdok= (sd.disk_initialize() == 0);
+//    if(!sdok) kernel->streams->printf("SDCard failed to initialize\r\n");
+
+//    #ifdef NONETWORK
+//        kernel->streams->printf("NETWORK is disabled\r\n");
+//    #endif
+
+//#ifdef DISABLEMSD
+//    // attempt to be able to disable msd in config
+//    if(sdok && !kernel->config->value( disable_msd_checksum )->by_default(false)->as_bool()){
+//        // HACK to zero the memory USBMSD uses as it and its objects seem to not initialize properly in the ctor
+//        size_t n= sizeof(USBMSD);
+//        void *v = AHB0.alloc(n);
+//        memset(v, 0, n); // clear the allocated memory
+//        msc= new(v) USBMSD(&u, &sd); // allocate object using zeroed memory
+//    }else{
+//        msc= NULL;
+//        kernel->streams->printf("MSD is disabled\r\n");
+//    }
+//#endif
 
 	// Create and add main modules
-	kernel->add_module( new Endstops() );
-	kernel->add_module( new Laser() );
-	//kernel->add_module( new ZProbe() );
-	//kernel->add_module( new RotaryDeltaCalibration() );
-	kernel->add_module( new MotorDriverControl(0) );
+//      kernel->add_module( new Player() );
 
-	// Create all Switch modules
-	SwitchPool *sp= new SwitchPool();
-	sp->load_tools();
-	delete sp;
+//    kernel->add_module( new CurrentControl() );
+//    kernel->add_module( new KillButton() );
+//    kernel->add_module( new PlayLed() );
 
-	// Create all TemperatureControl modules. Note order is important here must be after extruder so Tn as a parameter will get executed first
-	TemperatureControlPool *tp= new TemperatureControlPool();
-	tp->load_tools();
-	delete tp;
+    // these modules can be completely disabled in the Makefile by adding to EXCLUDE_MODULES
+    #ifndef NO_TOOLS_ENDSTOPS
+    kernel->add_module( new Endstops() );
+    #endif
 
-	// Clear the configuration cache as it is no longer needed
-	kernel->config->config_cache_clear();
+    #ifndef NO_TOOLS_SWITCH
+    SwitchPool *sp= new SwitchPool();
+    sp->load_tools();
+    delete sp;
+    #endif
+    #ifndef NO_TOOLS_EXTRUDER
+    // NOTE this must be done first before Temperature control so ToolManager can handle Tn before temperaturecontrol module does
+    ExtruderMaker *em= new ExtruderMaker();
+    em->load_tools();
+    delete em;
+    #endif
+    #ifndef NO_TOOLS_TEMPERATURECONTROL
+    // Note order is important here must be after extruder so Tn as a parameter will get executed first
+    TemperatureControlPool *tp= new TemperatureControlPool();
+    tp->load_tools();
+    delete tp;
+    #endif
+    #ifndef NO_TOOLS_LASER
+    kernel->add_module( new Laser() );
+    #endif
+    #ifndef NO_TOOLS_SPINDLE
+    kernel->add_module( new Spindle() );
+    #endif
+    #ifndef NO_UTILS_PANEL
+//    kernel->add_module( new Panel() );
+    #endif
+    #ifndef NO_TOOLS_ZPROBE
+    kernel->add_module( new ZProbe() );
+    #endif
+    #ifndef NO_TOOLS_SCARACAL
+//    kernel->add_module( newSCARAcal() );
+    #endif
+    #ifndef NO_TOOLS_ROTARYDELTACALIBRATION
+    kernel->add_module( new RotaryDeltaCalibration() );
+    #endif
+    #ifndef NONETWORK
+//    kernel->add_module( new Network() );
+    #endif
+    #ifndef NO_TOOLS_TEMPERATURESWITCH
+    // Must be loaded after TemperatureControl
+    kernel->add_module( new TemperatureSwitch() );
+    #endif
+    #ifndef NO_TOOLS_DRILLINGCYCLES
+    kernel->add_module( new Drillingcycles() );
+    #endif
+    #ifndef NO_TOOLS_FILAMENTDETECTOR
+    kernel->add_module( new FilamentDetector() );
+    #endif
+    #ifndef NO_UTILS_MOTORDRIVERCONTROL
+    kernel->add_module( new MotorDriverControl(0) );
+    #endif
+//    // Create and initialize USB stuff
+//    u.init();
 
+//#ifdef DISABLEMSD
+//    if(sdok && msc != NULL){
+//        kernel->add_module( msc );
+//    }
+//#else
+//    kernel->add_module( &msc );
+//#endif
+
+//    kernel->add_module( &usbserial );
+//    if( kernel->config->value( second_usb_serial_enable_checksum )->by_default(false)->as_bool() ){
+//        kernel->add_module( new(AHB0) USBSerial(&u) );
+//    }
+
+//    if( kernel->config->value( dfu_enable_checksum )->by_default(false)->as_bool() ){
+//        kernel->add_module( new(AHB0) DFU(&u));
+//    }
+
+//    // 10 second watchdog timeout (or config as seconds)
+//    float t= kernel->config->value( watchdog_timeout_checksum )->by_default(10.0F)->as_number();
+//    if(t > 0.1F) {
+//        // NOTE setting WDT_RESET with the current bootloader would leave it in DFU mode which would be suboptimal
+//        kernel->add_module( new Watchdog(t*1000000, WDT_MRI)); // WDT_RESET));
+//        kernel->streams->printf("Watchdog enabled for %f seconds\n", t);
+//    }else{
+        kernel->streams->printf("WARNING Watchdog is disabled\n");
+//    }
+
+
+//    kernel->add_module( &u );
+
+    // memory before cache is cleared
+    //SimpleShell::print_mem(kernel->streams);
+
+    // clear up the config cache to save some memory
+    kernel->config->config_cache_clear();
+
+    if(kernel->is_using_leds()) {
+        // set some leds to indicate status... led0 init done, led1 mainloop running, led2 idle loop running, led3 sdcard ok
+        leds[0]= 1; // indicate we are done with init
+        leds[3]= sdok?1:0; // 4th led indicates sdcard is available (TODO maye should indicate config was found)
+    }
+
+    if(sdok) {
+        // load config override file if present
+        // NOTE only Mxxx commands that set values should be put in this file. The file is generated by M500
+        FILE *fp= fopen(kernel->config_override_filename(), "r");
+        if(fp != NULL) {
+            char buf[132];
+            kernel->streams->printf("Loading config override file: %s...\n", kernel->config_override_filename());
+            while(fgets(buf, sizeof buf, fp) != NULL) {
+                kernel->streams->printf("  %s", buf);
+                if(buf[0] == ';') continue; // skip the comments
+                struct SerialMessage message= {&(StreamOutput::NullStream), buf};
+                kernel->call_event(ON_CONSOLE_LINE_RECEIVED, &message);
+            }
+            kernel->streams->printf("config override file executed\n");
+            fclose(fp);
+        }
+    }
+
+    // start the timers and interrupts
+    THEKERNEL->conveyor->start(THEROBOT->get_number_registered_motors());
+    THEKERNEL->step_ticker->start();
+//TODO IS this still required?    THEKERNEL->slow_ticker->start();
+}
+
+int main()
+{
+    init();
+	int cnt = 0;
 	// Main loop
 	while(1){
 		if(THEKERNEL->is_using_leds()) {
