d778c2356e1c21c0a0122614eb5874e41fe2a4b2
Marcos Gomes <marcosfg@ua.pt>
2016-11-07 15:26:35 +0000

    Changed base freq

--- src/libs/StepTicker.cpp
+++ src/libs/StepTicker.cpp
@@ -36,7 +36,7 @@ StepTicker::StepTicker(){
     instance = this; // setup the Singleton instance of the stepticker
 
     uint32_t PCLK = SystemCoreClock;
-    uint32_t prescale = PCLK / 10000000;
+    uint32_t prescale = PCLK / 100000000;
 
     /* Enable timer 0 clock and reset it */
     LPC_CCU1->CLKCCU[CLK_MX_TIMER0].CFG |= 1;
@@ -96,7 +96,7 @@ void StepTicker::start()
 void StepTicker::set_frequency( float frequency )
 {
     this->frequency = frequency;
-    this->period = floorf((SystemCoreClock / 4.0F) / frequency); // SystemCoreClock/4 = Timer increments in a second
+    this->period = floorf((SystemCoreClock) / frequency); // SystemCoreClock = Timer increments in a second
     LPC_TIMER0->MR[0] = this->period;
     LPC_TIMER0->TCR = 3;  // Reset
     LPC_TIMER0->TCR = 1;  // start
@@ -105,7 +105,7 @@ void StepTicker::set_frequency( float frequency )
 // Set the reset delay, must be called after set_frequency
 void StepTicker::set_unstep_time( float microseconds )
 {
-    uint32_t delay = floorf((SystemCoreClock / 4.0F) * (microseconds / 1000000.0F)); // SystemCoreClock/4 = Timer increments in a second
+    uint32_t delay = floorf((SystemCoreClock) * (microseconds / 1000000.0F)); // SystemCoreClock = Timer increments in a second
     LPC_TIMER1->MR[0] = delay;
 
     // TODO check that the unstep time is less than the step period, if not slow down step ticker
